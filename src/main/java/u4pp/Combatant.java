80a522bfe2254ac6f207afa871f5ca097a727e8c
Junjian Liu <junjianliu@Junjians-MacBook-Air.local>
2022-12-06 08:59:54 -0800

    Combatant

---
+++ src/main/java/u4pp/Combatant.java
@@ -0,0 +1,28 @@
+package u4pp;
+
+public class Combatant {
+
+
+public String getName(){
+
+}
+public int getHealth(){
+
+}
+public int getMaxHealth(){
+
+}
+public int getAttackPower(){
+
+}
+public void setHealth(int health){
+
+}
+public void setMaxHealth(int health){
+
+}
+public void setAttackPower(int attackPower){
+    
+}
+
+}
---
+++ src/main/java/u4pp/MonsterFighter.java
@@ -0,0 +1,5 @@
+package u4pp;
+
+public class MonsterFighter {
+    
+}
