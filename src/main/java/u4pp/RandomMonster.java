69f0264cc39e7d4b7f38bab2c5df316998442048
pattonliu84 <113130211+pattonliu84@users.noreply.github.com>
2022-12-16 11:22:56 -0800

    Update RandomMonster.java

--- src/main/java/u4pp/RandomMonster.java
+++ src/main/java/u4pp/RandomMonster.java
@@ -1,5 +1,19 @@
 package u4pp;
 
-public class RandomMonster {
-    
+public class RandomMonster extends Monster {
+     
+    public RandomMonster (String name, int health, int attack, int exp){
+        super (name, health, attack, exp);
+    }
+//Hits half the time
+    public void takeTurn (Combatant target){
+        double rand = Math.random ();
+        if (rand > 0.5){
+            target.takeDamage(getAttackPower());
+        }
+    }
+
+    public String toString(){
+        return "The random monster who only takes turn 50% of the time " + getName() + " with health " + getHealth() + " and max health: " + getMaxHealth() + " has attack power: " + getAttackPower();
+    }
 }
