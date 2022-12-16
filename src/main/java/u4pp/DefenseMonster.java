77e5c3dd585967a379046122d3d4a25f9a6ca62f
pattonliu84 <113130211+pattonliu84@users.noreply.github.com>
2022-12-16 11:20:31 -0800

    Update DefenseMonster.java

--- src/main/java/u4pp/DefenseMonster.java
+++ src/main/java/u4pp/DefenseMonster.java
@@ -1,5 +1,28 @@
 package u4pp;
 
-public class DefenseMonster {
+public class DefenseMonster extends Monster{
+    private int monsterDefense;
+    public DefenseMonster (String name, int health, int attack, int exp, int defense){
+        super(name, health, attack, exp);
+        monsterDefense = defense;
+        }
+
+    public int getDefense(){
+        return monsterDefense;
+    }
+
+    //takesDamage, damage has to be greater than the defense for it to work
+    public void takeDamage (int damage){
+        if (damage - getDefense() > 0){
+            int i = damage - getDefense();
+        setHealth (health - i);
+        } else {
+            return;
+        }
+    }
+
+    public String toString (){
+        return "The defense monster " + getName() + "with health " + getHealth() + " and max health " + getMaxHealth() + " has attack power " + getAttackPower() + " with experience given " + getExpGiven() + " and defense" + getDefense();
+    }
     
 }
