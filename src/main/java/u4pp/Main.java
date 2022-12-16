469b9a40d6f5f8d7b0e7f3431b7c12189bf33aa4
pattonliu84 <113130211+pattonliu84@users.noreply.github.com>
2022-12-16 11:21:46 -0800

    Update Main.java

--- src/main/java/u4pp/Main.java
+++ src/main/java/u4pp/Main.java
@@ -1,8 +1,10 @@
 package u4pp;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("welcome to u4pp! Good luck :)\n"+
-        "(Please delete this line of code before submitting :) )");
+        Scanner scanner = new Scanner (System.in);
+        MonsterFighter myMonsterFighter = new MonsterFighter();
+        myMonsterFighter.play(scanner);
     }
 }
