d09587925f925f39d64dbec35d991957a8b33de4
pattonliu84 <113130211+pattonliu84@users.noreply.github.com>
2022-12-16 11:22:06 -0800

    Update Monster.java

--- src/main/java/u4pp/Monster.java
+++ src/main/java/u4pp/Monster.java
@@ -1,5 +1,30 @@
 package u4pp;
 
-public class Monster {
+public class Monster extends Combatant {
+
+    private int existMon;
+
+    public Monster(String name, int health, int attack, int exp){
+        super(name, health, attack);
+        if (exp < 0){
+            existMon = 0;
+        } else {
+            existMon = exp;
+        }
+    }
+
     
+    public int getExpGiven(){
+        return existMon;
+    }
+//Monster's actions
+    public void takeTurn(Combatant target){
+        target.takeDamage(getAttackPower());
+    }
+
+    public String toString (){
+        return getName() + " normal monster " + getAttackPower() + " " + getHealth() + " " + getMaxHealth() + " " + getExpGiven() + " ";
+    }
+
 }
+    
